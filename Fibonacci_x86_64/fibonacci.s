27a1f90a2d3e3ac0c0b0638ed59b5df0fb31b871
Aren <82329186+heishmanA@users.noreply.github.com>
2023-07-14 17:03:02 -0400

    Fibonnacci Sample Prog version and Encryption
    Fibonacci.s prints out the fibonnaci sequence up to the number the user gives
    
    Lab5 is an encryption and decryption algorithm

---
+++ Fibonacci_x86_64/fibonacci.s
@@ -0,0 +1,183 @@
+# Prints the fibonnaci sequence based on the user input 
+# Aaron Heishman
+# 7/8/2023
+
+.file "fibonacci.s" # file name
+.section .rodata    # read only
+
+out_format:     # closing string for printing results
+    .string "%i: %lu\n"
+
+u_long_f:          # unsigned long format
+    .string "%lu"
+
+.global main
+    .type main, @function
+
+.text
+main:
+    # set up stack
+    pushq   %rbp
+    movq    %rsp, %rbp
+    # save argument count and arguments before checking invalid input
+    pushq   %rsi
+    pushq   %rdi
+    # check for invalid input, %rdi and %rsi hold CLI/Argcount
+    call    check_input
+    # restore argument count and arguments
+    popq    %rdi
+    popq    %rsi
+
+    cmpq    $0, %rax        # compare the return value of check_input to 0
+    je      .quit           # jump if equal to - %rax contains 1 (error for check_input)
+    
+    # if rax returns 0, convert the second argument from string to long
+    # format = strloul (string, ptr to place in, base)
+    movq    $10, %rdx
+    movq    %r13, %rdi
+    movq    8(%rsi), %rdi
+
+    xor     %rax, %rax      # zero rax
+    call    strtoul         # convert the argument from a string to an unsigned long
+                            # rax will hold the unsigned long
+    movq    $1, %r12        # move zero to %r12 to represent the count
+                            # since fibonacci sequence starts at 1
+    movq    %rax, %r13      # move value to %r13
+
+    # loop from 0 to n printing the fibonnaci sequence in order
+    .fib_sequence_loop:
+        cmpq    %r13, %r12  # compare user input to current count
+        jg     .quit        # jump if greater than (count > n)
+        
+        pushq   %r13        # save user value
+        pushq   %r12        # save the count
+
+        movq    %r12, %rdi  # move count to %rdi to get f(count)
+        call    fib         # get fib(n)
+
+        popq    %r12        # restore count
+        popq    %r13        # restore count
+        
+        pushq   %r13        # save count
+        pushq   %r12        # save user val
+        movq    %rax, %rdx  # move the return value to the third param
+        movq    %r12, %rsi  # move the current count to the second param
+        movq    $out_format, %rdi # move the output message
+        xor     %rax, %rax  # zero return
+        call    printf  
+        popq    %r12        # restore user val
+        popq    %r13       # restore count
+
+        incq    %r12        # increment count by one
+        jmp     .fib_sequence_loop   # jump to beginning
+
+    .quit:
+        leave
+        ret
+.size main, .-main
+
+
+
+# Check for invalid input [No input, Empty Input, Negative, Non-Number]
+# return a 0 for fail, 1 for success
+invalid_msg:
+    .string "Usage: Please input the count of fibonacci numbers to output\n"
+.global check_input
+    .type check_input, @function
+.text
+check_input:
+    #set up stack frame for check_input
+    pushq   %rbp
+    movq    %rsp, %rbp
+    # the first parameter, %rdi, holds the arg count
+    # the second parameter, %rsi, holds the argument(s)
+    # Start by checking if input is empty
+    jmp .check_no_input
+
+    # Case No input: <myprog> [no args]
+    .check_no_input:
+        cmpq    $0x01, %rdi         # Compare arg count to 1
+        je      .print_error        # jump if equal (args count == 1)
+        jmp     .check_empty_input
+    
+    # Case Empty input: <myprog> ""
+    .check_empty_input: 
+        movq    8(%rsi), %rsi   # Move the first argument to %rsi
+        movb    (%rsi), %al     # move the first character to register %al
+        cmpb    $0x00, %al      # compare first character to null terminator byte
+        je      .print_error    # jump if equal (empty input)
+        # if equal flag not set, then check the rest of the characters for valid input
+        movq    $0, %r12        # move 0 to %r12 to use %r12 as a count for the loop
+        jmp     .check_for_characters
+        
+    # Case Invalid input, not a number:
+    .check_for_characters:
+        movb    (%r12, %rsi), %al   # move the character to %al
+        # Check if the character is null byte first
+        cmpb    $0x00, %al          # compare character to null byte character
+        je      .success            # jump if equal to
+
+        # Compare the character to see if it falls in the range of [0, 9]
+        cmp     $0x30, %al          # compare the character to 0
+        jl      .print_error        # jump if less than (character is not a digit)
+
+        cmp     $0x39, %al          # compare the character to 9
+        jg      .print_error        # jump if greater than (character is not a digit)
+
+        incq    %r12                # increment %r12 by one
+
+        jmp     .check_for_characters
+
+    .print_error:
+        movq    $invalid_msg, %rdi  # move the error message to register
+        xor     %rax, %rax          # zero return
+        call    printf              # print error message
+        movq    $0, %rax            # put error code in movq
+        jmp     .leave
+    
+    .success:
+        movq $0, %rax   # return 0 if complete
+
+    .leave:
+        leave
+        ret
+.size check_input, .-check_input
+
+# Start of the fibonnaci sequence function
+# fib(u_long n)
+.global fib
+    .type fib, @function
+.text
+fib:
+    # set up the stack
+    pushq   %rbp
+    movq    %rsp, %rbp
+
+    cmpq    $2, %rdi        # rdi holds n, compare n : 2
+    jb      .fib_base       # base case n < 2 - jump to .fib_base and return n
+                            # else n >= 2, so recursive step
+
+    pushq   %rdi            # save the original value of %rdi (n)
+
+    subq    $1, %rdi        # decrement n by 1 (n-1)
+    call    fib             # call fib with (n-1)
+    movq    %rax, %r9       # store the return value of fib(n-1) in %r9
+    popq    %rdi            # restore the original value of rdi (n)
+
+    subq    $2, %rdi        # decrement rdi by 2 to do n-2 since %r9's original value was saved
+    pushq   %r9             # save %r9 as it holds return of fib(n-1)
+                            # no need to save %rdi as it's value is no longer needed
+    call    fib             # call fib with (n-2)
+    popq    %r9             # restore %r9
+    addq    %r9, %rax       # add result of fib(n-1) to fib(n-2) and return that value
+
+    # done with recursion, return rax
+    leave
+    ret
+    
+    .fib_base: # label for base case n < 2
+        movq   %rdi, %rax # copy %rdi to %rax (putting n in return value)
+        leave
+        ret
+
+.size fib, .-fib
---
+++ Lab5/Lab5Encrypt.s
@@ -0,0 +1,171 @@
+# Aaron Heishman
+# Lab 5 - Systems I
+# Lab5Encrypt file
+.file "Lab5Encrypt.s"
+.section .rodata
+
+.string_f:
+    .string "%s"
+.data
+.align 8
+# Array to hold all characters, initalized as null ['0x0']
+.encryptee_arr:   
+    .byte 0x0
+    .byte 0x0
+    .byte 0x0
+    .byte 0x0
+    .byte 0x0
+    .byte 0x0
+    .byte 0x0
+    .byte 0x0
+    
+.global main
+    .type main, @function
+
+.text
+main:
+    # stack setup
+    pushq   %rbp
+    movq    %rsp, %rbp
+    # Set up "variables"
+    movq    $0, %r12                    # load '0' in r12b as r12b will represent both
+                                        # the count and current index of the array
+    movq    $.encryptee_arr, %r13       # load the address of encryptee char into %r12
+   
+    # Read in 8 chars at a time
+    .read:
+        # save original values of %r12d, %r13d, %r14d
+        pushq   %r12
+        pushq   %r13
+        # Read in the character 
+        xor     %rax, %rax      # zero out the entire value of rax
+        call    getchar         # get the character
+
+        # Restore all values before checking if next char is EOF
+        popq    %r13
+        popq    %r12
+
+        # check for EOF value
+        cmpl    $-1, %eax     # Check if EOF is returned
+        je      .check        # jump to check to make sure if the array has any values
+        movb    %al, (%r13, %r12, 1)    # move the lowest byte from %al to r13[count]
+        incq    %r12           # increment count/index by 1
+        cmpq    $8, %r12      # compare count : 8
+        jl      .read         # jump if less than (count < 7)
+        je      .encrypt      # jump if equal to (count == 7)
+    
+    .encrypt:
+        # This is where the encrypt will be called, and count reset
+        # for now it's for debugging
+        pushq   %r12               # save the count
+        pushq   %r13               # save the address of the the array
+        movzbq  7(%r13), %rsi      # move the 8th character to the second parameter
+        movq    %r13, %rdi         # move the char array (or string) to second param
+        call    encrypt            # encrypt 7 characters found in .encryptee_arr by the character now in %rsi
+        popq    %r13               # restore the address of the array
+        popq    %r12               # restore the count
+        xor     %r12, %r12         # zero out the count
+        movq    $0, (%r13)         # replace all bytes in .encryptee_arr with 0x0
+        jmp     .read              # loop again to check for more chars
+
+    .check:  # check to make sure if %rsi has any unencrypted chars to be placed into the document
+        cmpq $0, %r12   # compare the count : 0
+        je   .end       # jump if equal (count == 0 no chars in array)
+        # print the last characters read in unencrypted
+        movq    %r13, %rsi          # move the character array into the second parameter
+        movq    $.string_f, %rdi    # move the format specifier into first param
+        xor     %rax, %rax          # zero return
+        call    printf              # print the array
+
+    .end:
+        xor %rax, %rax  # zero out return to signify a successful run
+        leave
+        ret
+.size main, .-main
+
+# Encrypts the array that will passed to %rdi
+.global encrypt
+    .type encrypt, @function
+
+encrypt:
+    # stack set up
+    pushq   %rbp
+    movq    %rsp, %rbp
+    subq    $8, %rsp        # allocate space on the stack
+    
+    # RCX -> CL -> use for our variable bit for shifting
+    movq    %rsi, %rax      # 8th character was in param two, move to rax
+    # the array is in %r12
+    movq    $0, %r13        # %r13 used as "index"
+    movq    $7, %r14        # %r14 will count as "k" 
+
+
+    # Encrypt each character in %rsi with a bit from the character in  %rax
+    .encrypt_chars:     # start of loop
+        # save all the values
+        pushq   %rax    # save encryptor character (8th character)
+        pushq   %r13    # save index/count
+        pushq   %r14    # save "k"
+        pushq   %rdi    # save the array
+
+        # Get the "ith" bit from encryptor character as it is on the right
+        movq    $0x01, %rbx     # move "0000 0001" into %rbx
+        movq    %r13, %rcx      # move "i" into %rcx
+        shlq    %cl, %rbx       # shift %rbx by "index"
+        andq    %rbx, %rax      # and %rbx and %rax to get the "ith" bit
+
+        # Get the [0, k] bits from the character to be encrypted
+        movq    (%rdi, %r13, 1), %rdx   # move the encryptee char to %rdx [rdx = %rdi[i]]
+        movq    $0xFF, %rbx     # move "1111 1111" into rbx
+        movq    %r14, %rcx      # move "k" into %rcx
+        shlq    %cl, %rbx       # shift %rbx "k" bits [rbx << k]
+        andq    %rbx, %rdx      # and %rbx and %rdx to get the [0, k] bits
+        cmpq    $0, %r13        # compare [i : 0]
+        je      .encrypt_chars_cont # jump if equalk to (i == 0)
+        jg      .encrypt_shl_char  # jump if greater than (i > 0)
+        
+
+    # Shift characters [2, 6] left by one to make space for encryptor bit
+    .encrypt_shl_char:
+        shlq     $1, %rdx    # shift %rdx "i" bits [rdx << 1]
+
+    # Continue with encryption
+    .encrypt_chars_cont:    
+        # This combination of moves will move rax the "difference" between i and k (k - i) 
+        movq    %r13, %rcx      # move "i" into %rcx
+        shrq    %cl, %rax       # shift %rax "i" bits [%rax >> i] - bit is not at pos 7
+        movq    %r14, %rcx      # move k to %rcx
+        shlq    %cl, %rax       # shift %rax "k" bits [%rax << k] - bit is now at the k position
+        orq     %rax, %rdx      # add the byte at the "k" position to %rdx 
+        movq    %rdx, %r15      # store the left side into %r15
+
+        # get the right sided bits of the character
+        movq    $0xFF, %rbx             # move "1111 1111" to %rbx
+        movq    (%rdi, %r13, 1), %rdx   # restore the character to get the right side
+        movq    %r13, %rcx              # move "i" to rcx
+        incq    %rcx                    # increment "i" by one to not get the bit that was shifted left one
+        shrq    %cl, %rbx               # shift %rbx "i" times
+        andq    %rbx, %rdx              # and %rbx and %rdx to zero the left side of the bits to replace
+        orq     %r15, %rdx              # combine %r15 and %rdx to create the encrypted char
+
+    # put the encrypted character into the file or console
+    .encrypt_put_char:      # label for easier reading
+        movq    %rdx, %rdi  # move the encrypted character to the first param
+        xor     %rax, %rax  # zero return
+        call    putchar     # print char
+        popq    %rdi        # restore the array
+        popq    %r14        # restore bit "k"
+        popq    %r13        # restore the "i"
+        popq    %rax        # restore the encryptor character
+        incq    %r13        # increment "i" by 1
+        decq    %r14        # decrement "k" by 1
+        cmpq    $7, %r13    # check if (i < 7)
+        jl     .encrypt_chars # jump if less than (%r13 < 7)
+
+    # leave function
+    .encrypt_end:       # label for easier reading
+        addq $8, %rsp   # deallocate the space made for the stack
+        xor %rax, %rax  # zero out return to signify completion
+        leave
+        ret
+.size main, .-main
